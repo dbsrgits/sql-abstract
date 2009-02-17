bffe85752eb6bd3736efb26d53d3cfe2eada99f1
Norbert Buchmuller <norbi@nix.hu>
2009-02-17 19:58:31 +0000

    Bumped version number to 1.49_03.

--- lib/SQL/Abstract.pm
+++ lib/SQL/Abstract.pm
@@ -15,7 +15,7 @@ use Scalar::Util qw/blessed/;
 # GLOBALS
 #======================================================================
 
-our $VERSION  = '1.49_02';
+our $VERSION  = '1.49_03';
 $VERSION      = eval $VERSION; # numify for warning-free dev releases
 
 
