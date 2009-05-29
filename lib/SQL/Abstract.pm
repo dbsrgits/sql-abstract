f267b646207361aa110bff7df661b5881b5c8319
Peter Rabbitson <ribasushi@cpan.org>
2009-05-29 15:41:10 +0000

    Fix for _order_by with bind values - will not work on DBIC - needs matching changes to SQLAHacks

--- lib/SQL/Abstract.pm
+++ lib/SQL/Abstract.pm
@@ -813,48 +813,77 @@ sub _where_field_IN {
 sub _order_by {
   my ($self, $arg) = @_;
 
-  # construct list of ordering instructions
-  my @order = $self->_SWITCH_refkind($arg, {
+  my (@sql, @bind);
+  for my $c ($self->_order_by_chunks ($arg) ) {
+    $self->_SWITCH_refkind ($c, {
+      SCALAR => sub { push @sql, $c },
+      ARRAYREF => sub { push @sql, shift @$c; push @bind, @$c },
+    });
+  }
+
+  my $sql = @sql
+    ? sprintf ('%s %s',
+        $self->_sqlcase(' order by'),
+        join (', ', @sql)
+      )
+    : ''
+  ;
+
+  return wantarray ? ($sql, @bind) : $sql;
+}
+
+sub _order_by_chunks {
+  my ($self, $arg) = @_;
+
+  return $self->_SWITCH_refkind($arg, {
 
     ARRAYREF => sub {
-      map {$self->_SWITCH_refkind($_, {
-              SCALAR    => sub {$self->_quote($_)},
-              UNDEF     => sub {},
-              SCALARREF => sub {$$_}, # literal SQL, no quoting
-              HASHREF   => sub {$self->_order_by_hash($_)}
-             }) } @$arg;
+      map { $self->_order_by_chunks ($_ ) } @$arg;
     },
 
+    ARRAYREFREF => sub { [ @$$arg ] },
+
     SCALAR    => sub {$self->_quote($arg)},
-    UNDEF     => sub {},
+
+    UNDEF     => sub {return () },
+
     SCALARREF => sub {$$arg}, # literal SQL, no quoting
-    HASHREF   => sub {$self->_order_by_hash($arg)},
 
+    HASHREF   => sub {
+      # get first pair in hash
+      my ($key, $val) = each %$arg;
+
+      return () unless $key;
+
+      if ( (keys %$arg) > 1 or not $key =~ /^-(desc|asc)/i ) {
+        puke "hash passed to _order_by must have exactly one key (-desc or -asc)";
+      }
+
+      my $direction = $1;
+
+      my (@sql, @bind);
+      for my $c ($self->_order_by_chunks ($val)) {
+
+
+
+        $self->_SWITCH_refkind ($c, {
+          SCALAR => sub {
+            push @sql, $c
+          },
+          ARRAYREF => sub {
+            my ($s, @b) = @$c;
+            push @sql, $s;
+            push @bind, @b;
+          },
+        });
+      }
+
+      my $sql = join ', ', map { $_ . ' ' . $self->_sqlcase($direction) } @sql;
+
+      return [$sql, @bind];
+    },
   });
-
-  # build SQL
-  my $order = join ', ', @order;
-  return $order ? $self->_sqlcase(' order by')." $order" : '';
 }
-
-
-sub _order_by_hash {
-  my ($self, $hash) = @_;
-
-  # get first pair in hash
-  my ($key, $val) = each %$hash;
-
-  # check if one pair was found and no other pair in hash
-  $key && !(each %$hash)
-    or puke "hash passed to _order_by must have exactly one key (-desc or -asc)";
-
-  my ($order) = ($key =~ /^-(desc|asc)/i)
-    or puke "invalid key in _order_by hash : $key";
-
-  $val = ref $val eq 'ARRAY' ? $val : [$val];
-  return join ', ', map { $self->_quote($_) . ' ' . $self->_sqlcase($order) } @$val;
-}
-
 
 
 #======================================================================
