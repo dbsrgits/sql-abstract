0f2b8636560c4634ec2c73116b0a3dbef2052405
Peter Rabbitson <ribasushi@cpan.org>
2009-09-22 11:03:37 +0000

    Release 1.60

--- lib/SQL/Abstract.pm
+++ lib/SQL/Abstract.pm
@@ -15,7 +15,7 @@ use Scalar::Util qw/blessed/;
 # GLOBALS
 #======================================================================
 
-our $VERSION  = '1.59';
+our $VERSION  = '1.60';
 
 # This would confuse some packagers
 #$VERSION      = eval $VERSION; # numify for warning-free dev releases
