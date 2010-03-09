f25326299449cab0badd546e118558bb24fb12e7
Peter Rabbitson <ribasushi@cpan.org>
2010-03-09 12:33:24 +0000

    Bump trunk version

--- lib/SQL/Abstract.pm
+++ lib/SQL/Abstract.pm
@@ -15,7 +15,7 @@ use Scalar::Util ();
 # GLOBALS
 #======================================================================
 
-our $VERSION  = '1.61';
+our $VERSION  = '1.61_01';
 
 # This would confuse some packagers
 #$VERSION      = eval $VERSION; # numify for warning-free dev releases
