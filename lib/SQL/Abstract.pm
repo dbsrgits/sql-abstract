7af9586d2527c93257f58ef980d503934db134ab
Arthur Axel "fREW" Schmidt <frioux@gmail.com>
2010-05-31 14:26:39 +0000

    for people working from repository

--- lib/SQL/Abstract.pm
+++ lib/SQL/Abstract.pm
@@ -15,7 +15,7 @@ use Scalar::Util ();
 # GLOBALS
 #======================================================================
 
-our $VERSION  = '1.67';
+our $VERSION  = '1.67_01';
 
 # This would confuse some packagers
 $VERSION = eval $VERSION if $VERSION =~ /_/; # numify for warning-free dev releases
