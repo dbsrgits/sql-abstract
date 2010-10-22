9f7f28c4d2eccc1b5fe0280577ad73d82916ac5d
Arthur Axel "fREW" Schmidt <frioux@gmail.com>
2010-10-22 14:32:27 +0000

    release SQL::Abstract 1.69

--- lib/SQL/Abstract.pm
+++ lib/SQL/Abstract.pm
@@ -15,7 +15,7 @@ use Scalar::Util ();
 # GLOBALS
 #======================================================================
 
-our $VERSION  = '1.68';
+our $VERSION  = '1.69';
 
 # This would confuse some packagers
 $VERSION = eval $VERSION if $VERSION =~ /_/; # numify for warning-free dev releases
