d7d3d1584c95a2bc501e9b783678fae97ac0bb57
Peter Rabbitson <ribasushi@cpan.org>
2010-11-09 12:20:51 +0100

    Release 1.71

--- lib/SQL/Abstract.pm
+++ lib/SQL/Abstract.pm
@@ -15,7 +15,7 @@ use Scalar::Util ();
 # GLOBALS
 #======================================================================
 
-our $VERSION  = '1.70';
+our $VERSION  = '1.71';
 
 # This would confuse some packagers
 $VERSION = eval $VERSION if $VERSION =~ /_/; # numify for warning-free dev releases
