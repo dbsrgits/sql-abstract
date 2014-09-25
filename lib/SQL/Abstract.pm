0ca23f3b8e7614421c0ea9d2bf04bc060e7c7ec5
Peter Rabbitson <ribasushi@cpan.org>
2014-09-25 14:00:59 +0200

    Release 1.79

--- lib/SQL/Abstract.pm
+++ lib/SQL/Abstract.pm
@@ -27,7 +27,7 @@ BEGIN {
 # GLOBALS
 #======================================================================
 
-our $VERSION  = '1.78';
+our $VERSION  = '1.79';
 
 # This would confuse some packagers
 $VERSION = eval $VERSION if $VERSION =~ /_/; # numify for warning-free dev releases
