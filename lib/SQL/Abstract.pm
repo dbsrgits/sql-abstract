f0724630077f85e0127fc4cb51e108c484feac61
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2017-02-28 19:09:42 +0000

    Release 1.81_01

--- lib/SQL/Abstract.pm
+++ lib/SQL/Abstract.pm
@@ -27,7 +27,7 @@ BEGIN {
 # GLOBALS
 #======================================================================
 
-our $VERSION  = '1.81';
+our $VERSION  = '1.81_01';
 
 # This would confuse some packagers
 $VERSION = eval $VERSION if $VERSION =~ /_/; # numify for warning-free dev releases
