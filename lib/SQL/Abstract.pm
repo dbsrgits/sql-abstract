64eae6a8efe2316258e78e200e50cbaff6ab10fd
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2017-04-03 16:16:15 +0100

    Bumping version to 1.83

--- lib/SQL/Abstract.pm
+++ lib/SQL/Abstract.pm
@@ -27,7 +27,7 @@ BEGIN {
 # GLOBALS
 #======================================================================
 
-our $VERSION  = '1.82';
+our $VERSION  = '1.83';
 
 # This would confuse some packagers
 $VERSION = eval $VERSION if $VERSION =~ /_/; # numify for warning-free dev releases
