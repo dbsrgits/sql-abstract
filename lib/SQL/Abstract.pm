df5aaa4c64eaf12d6d39a4b032cb8afc4abd246c
Matt S Trout <mst@shadowcat.co.uk>
2019-10-13 17:00:30 +0000

    Bumping version to 1.90_03

--- lib/SQL/Abstract.pm
+++ lib/SQL/Abstract.pm
@@ -28,7 +28,7 @@ BEGIN {
 # GLOBALS
 #======================================================================
 
-our $VERSION  = '1.90_02';
+our $VERSION  = '1.90_03';
 
 # This would confuse some packagers
 $VERSION = eval $VERSION if $VERSION =~ /_/; # numify for warning-free dev releases
